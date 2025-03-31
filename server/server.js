197aa9ae75b14be9c76550b27d6dab54fae28921
mynameist04st3r <t04st3r@mac.com>
2025-03-31 17:01:56 -0400

    Added migrations file.

--- server/server.js
+++ server/server.js
@@ -2,15 +2,19 @@ const port = 8000;
 const express = require('express');
 const app = express();
 const knex = require('knex')(require('./knexfile.js')['development']);
+
+
 app.get('/', (req, res) => {
   res.send('Here it is.');
 })
-app.get('/DATABASE_NAME', (req, res) => {
-  knex('DATABASE_NAME').select('*').then(DATABASE_HEADER => {
-    let VARIABLE = DATABASE_HEADER.map(DATABASE_NAME => DATABASE_NAME.DATABASE_HEADER)
-    res.json(VARIABLE);
+
+app.get('/movies', (req, res) => {
+  knex('movies').select('*').then(title => {
+    let movieTitles = title.map(movies => movies.title)
+    res.json(movieTitles);
   })
 });
+
 app.listen(port, () => {
   console.log('Server running at http://localhost: ' + port);
 });
